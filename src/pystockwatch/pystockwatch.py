2874fb584347d79803fbf01312cc17791507e51b
Affrin Sultana <affrin.sultana7@gmail.com>
2022-01-14 01:30:47 -0800

    added  function spec for profit viz

--- src/pystockwatch/pystockwatch.py
+++ src/pystockwatch/pystockwatch.py
@@ -1,3 +1,6 @@
+# author: Affrin Sultana,Helin Wang, Shi Yan Wang and Pavel Levchenko
+# date: 2022-14-01
+
 import plotly.express as px
 import pandas as pd
 import numpy as np
@@ -10,10 +13,32 @@ def (stock_ticker, start_date, end_date):
 
 
 
-def (stock_ticker, start_date, end_date, benchmark_ticker):
-#Function 2 for line plot visualization by Affrin
+def profit_viz(stock_ticker, start_date, end_date, benchmark_ticker):
 
-
+    """
+    Creates line plot showing the percentage change in profit for a given stock ticker in comparison to the benchmark ticker for a given date range
+    
+    Parameters
+    ----------
+    stock_ticker: string
+        Ticker of the stock such as 'AAPL'
+    start_date: datetime
+        Initial date for data extraction
+    end_date: datetime 
+        Final data for stock analysis
+    benchmark_ticker: string
+        Ticker with which comparison is to be done such as 'SPX' 
+    
+    Returns
+    --------
+    interactive line plots which shows the percentage change in profit over time 
+    
+    
+    Examples
+    --------
+    profit_viz('AAPL', '01-01-2015', '01-01-2022', 'SPX')
+    
+    """
 
 
 
