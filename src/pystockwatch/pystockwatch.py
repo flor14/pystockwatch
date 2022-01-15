7d63427811742aa230a6d19e17923372f8720487
Pavel Levchenko <plevchen@student.ubc.ca>
2022-01-15 11:51:56 -0800

    docs: minor changes in style of docstrings for consistency

--- src/pystockwatch/pystockwatch.py
+++ src/pystockwatch/pystockwatch.py
@@ -1,5 +1,4 @@
-# author: Affrin Sultana,Helin Wang, Shi Yan Wang and Pavel Levchenko
-# date: 2022-14-01
+# authors: Affrin Sultana, Helin Wang, Shi Yan Wang and Pavel Levchenko
 
 import plotly.express as px
 import pandas as pd
@@ -7,116 +6,118 @@ import numpy as np
 import yfinance as yf
 
 
-def increase_pencent(stock_ticker, start_date, end_date):
-#Function 1 for percentage change calculation by Helin
+def percent_change(stock_ticker, start_date, end_date):
     """
-    Calculate the stock price increase percentage by using end_date price minus start_date price and divided by the start_date price.
+    Calculates daily percentage change of a stock price within a given period of time
     
     Parameters
     ----------
-    stock_ticker: string
+    stock_ticker : string 
         Ticker of the stock such as 'AAPL'
-    start_date: datetime
+    start_date : string
         Initial date for data extraction
-    end_date: datetime 
-        Final data for stock analysis
+    end_date : string
+        Final date for stock analysis
     
     Returns
     --------
-    The value of stock profit increase percentage.
-    
+    DataFrame
+        A data frame with dates and their corresponding stock price percentage changes.
     
     Examples
     --------
-    >>>increase_percent('AAPL', '01-01-2021', '01-02-2021')
-     
-    """
-
+    >>> percent_change('AAPL', '01-01-2021', '12-31-2021')
+        Date            Price Change Percentage(%)
+        01-01-2021      0
+        01-02-2021      0.7
+        01-03-2021      1.1
+        ...
+        12-30-2021      23
+        12-31-2021      6.5
+    """ 
+    pass
+    # TODO
 
 
 def profit_viz(stock_ticker, start_date, end_date, benchmark_ticker):
-
     """
-    Creates line plot showing the percentage change in profit for a given stock ticker in comparison to the benchmark ticker for a given date range
+    Visualizes trend of a stock price change against the market benchmark within a given period of time
     
     Parameters
     ----------
-    stock_ticker: string
+    stock_ticker : string
         Ticker of the stock such as 'AAPL'
-    start_date: datetime
+    start_date : string 
         Initial date for data extraction
-    end_date: datetime 
-        Final data for stock analysis
-    benchmark_ticker: string
-        Ticker with which comparison is to be done such as 'SPX' 
+    end_date : string
+        Final date for stock analysis
+    benchmark_ticker : string 
+        Ticker for benchmark comparison such as 'SPX' 
     
     Returns
     --------
-    interactive line plots which shows the percentage change in profit over time 
-    
+    Interactive line plots which shows percentage change in stock price and market performance over time 
     
     Examples
     --------
-    profit_viz('AAPL', '01-01-2015', '01-01-2022', 'SPX')
-    
-    """
-
-
-
-def volumeChange(stock_ticker, start_date, end_date):
-    """ 
-    Return a dataframe that indicates daily volume change against previous day for the given time period
-
-    Parameters
-    ----------
-    stock_ticker: string
-        Ticker of the stock such as 'AAPL'
-    start_date: datetime
-        Initial date for data extraction
-    end_date: datetime 
-        Final data for stock analysis
-
-
-    Returns:
-        [pandas.core.frame.DataFrame]: [A Pandas dataframe contains indicators of daily volume change]
-
-    Examples:
-        >>> volumeChange('AAPL', '01-01-2015', '01-01-2022)
-        0       Nan
-        1       Increase
-        2       Increase
-        3       Decrease
-        ...
-        2255    Increase
-        2256    Increase
+    >>> profit_viz('AAPL', '01-01-2015', '01-01-2022', 'SPX')
     """
     pass
     # TODO
 
 
+def volume_change(stock_ticker, start_date, end_date):
+    """ 
+    Calculate the daily trading volume change status of a stock within a given period of time
 
+    Parameters
+    ----------
+    stock_ticker : string 
+        Ticker of the stock such as 'AAPL'
+    start_date : string
+        Initial date for data extraction
+    end_date : string 
+        Final date for stock analysis
+        
+    Returns
+    --------
+        A data frame with dates and their corresponding trading volume changes
+
+    Examples
+    --------
+        >>> volume_change('AAPL', '01-01-2021', '01-01-2022)
+        01-01-2021       Nan
+        01-02-2021       Increase
+        01-03-2021       Increase
+        01-04-2021       Decrease
+        ...
+        12-31-2021       Increase
+        01-01-2022       Increase
+    """
+    pass
+    # TODO
+    
 
 def volume_viz(stock_ticker, start_date, end_date):
     """
-    Creates bar plot with trading volumes for a given stock colored by changes in volume, where increased volume is colored by green and decreased volume is colored by reduce
-    
+    Visualize the daily trading volume of a stock using bar plot within a given period of time
+
     Parameters
     ----------
-    stock_ticker: string
+    stock_ticker : string  
         Ticker of the stock such as 'AAPL'
-    start_date: datetime
+    start_date : string 
         Initial date for data extraction
-    end_date: datetime 
-        Final data for stock analysis
+    end_date : string 
+        Final date for stock analysis
     
     Returns
     --------
-    interactive plot with overlay of line plots and bar plot
-    
+    Interactive plot with overlay of line plots and bar plot
     
     Examples
     --------
-    volume_viz('AAPL', '01-01-2015', '01-01-2022')
-     
+    >>> volume_viz('AAPL', '01-01-2015', '01-01-2022')
     """
     pass
+    # TODO
