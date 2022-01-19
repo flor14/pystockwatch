ddcad0baab1480fdd360fda93f707719958f039f
sy25wang <90230188+sy25wang@users.noreply.github.com>
2022-01-19 13:02:14 -0800

    Add tests to change_volume function

--- tests/test_pystockwatch.py
+++ tests/test_pystockwatch.py
@@ -1,2 +1,41 @@
 from pystockwatch import pystockwatch
 from pytest import raises
+
+def test_percentchange():
+  pass
+
+
+def test_profit_viz():
+  pass
+
+
+def test_volume_change():
+    # Invalid stock ticker
+    test_1 = "invalid ticker"
+    with raises(NameError) as error_ticker:
+        volume_change(test_1, "2021-12-01", "2021-12-31")
+    assert(
+        str(error_ticker.value) == "You enter an invalid stock ticker! Try again.")
+    
+    # Invalid date
+    test_2 = "invalid start_date"
+    with raises(ValueError) as error_start_date:
+        volume_change("A", "2021", "2021-01-01")
+    assert(
+        str(error_start_date.value) == "You enter an invalid start date! Try again.")
+    
+    test_3 = "invalid_end_date"
+    with raises(ValueError) as error_end_date:
+        volume_change("A", "2021-01-01", 100)
+    assert(
+        str(error_end_date.value) == "You enter an invalid end date! Try again.")
+
+    # Test indicators values
+    indicators = ['nan', 'Decrease', 'Increase']
+    df = volume_change("A", "2017-01-01", "2017-01-01")
+    df_unique = df["Volume_Change"].unique()
+    assert set(df_unique) <= set(indicators)
+    
+
+def volume_viz():
+  pass
