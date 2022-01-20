19ae8bab7415b671cc9c69d6fc1f1c89e2ad8021
Helin Wang <helena.wang.2021@gmail.com>
2022-01-19 17:20:07 -0800

    add function 1

--- src/pystockwatch/pystockwatch.py
+++ src/pystockwatch/pystockwatch.py
@@ -9,7 +9,6 @@ import pandas_datareader as pdr
 import datetime
 import warnings
 
-
 def percent_change(stock_ticker, start_date, end_date):
     """
     Calculates daily percentage change of a stock price within a given period of time
@@ -17,7 +16,7 @@ def percent_change(stock_ticker, start_date, end_date):
     Parameters
     ----------
     stock_ticker : string 
-        Ticker of the stock such as 'AAPL'
+        Ticker of the stock such as 'AAPL', or 'AAPL MSFT SPY' for multiple tickers
     start_date : string
         Initial date for data extraction
     end_date : string
@@ -30,17 +29,42 @@ def percent_change(stock_ticker, start_date, end_date):
     
     Examples
     --------
-    >>> percent_change('AAPL', '01-01-2021', '12-31-2021')
-        Date            Price Change Percentage(%)
-        01-01-2021      0
-        01-02-2021      0.7
-        01-03-2021      1.1
-        ...
-        12-30-2021      23
-        12-31-2021      6.5
+    >>> percent_change('AAPL', '2017-01-01', '2017-01-10')
+                    Price Change Percentage(%) 
+              Date
+        2017-01-03                      0.0000
+        2017-01-04                     -0.1119
+        2017-01-05                      0.3960
+        2017-01-06                      1.5153
+        2017-01-09                      2.4451
+    >>> percent_change('AAPL MSFT', '2017-01-01', '2017-01-10')
+                    Price Change Percentage(%) 
+                              AAPL        MSFT
+              Date
+        2017-01-03          0.0000      0.0000
+        2017-01-04         -0.1119     -0.4474
+        2017-01-05          0.3960     -0.4474
+        2017-01-06          1.5153      0.4155
+        2017-01-09          2.4451      0.0959
     """ 
-    pass
-    # TODO
+    
+    # Import original dataframe by giving stock ticker, start data and end date
+    data = yf.download(stock_ticker, start=start_date, end=end_date)
+    
+    # Only Keep "Adj Close" Price for 
+    data = data.drop(columns={'Open', 'High', 'Low', 'Close', 'Volume'})
+    
+    # Carry out calculation
+    for i in range(1,len(data)):
+        data.iloc[i,:] = (data.iloc[i,:] - data.iloc[0,:])/data.iloc[0,:]*100
+    
+    data.iloc[0,:] = (data.iloc[0,:] - data.iloc[0,:])/data.iloc[0,:]*100
+    
+    # Manipulate column name
+    data = data.rename(columns={"Adj Close": "Price Change Percentage(%)"})
+    
+    # Return result
+    return pd.DataFrame(data)
 
 
 def profit_viz(stock_ticker, start_date, end_date, benchmark_ticker):
