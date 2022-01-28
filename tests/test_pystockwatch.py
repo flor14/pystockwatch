d0c6e085a6dbfbfc7f50102577988a26e5cb31ff
plevchen <p.levchenko@gmail.com>
2022-01-28 15:59:29 -0700

    "Price_change" in func4 minor update

--- tests/test_pystockwatch.py
+++ tests/test_pystockwatch.py
@@ -103,7 +103,7 @@ def test_volume_change():
 def test_volume_viz():
     vdf = volume_change('AAPL', '2015-01-01', '2016-01-01')
     assert vdf.shape[0] >= 1, 'dataframe should have at least one row'
-    assert list(vdf.columns) == ['Date', 'Volume', 'Price_Change'], "columns should be named 'Date', 'Volume', 'Price_Change'"
+    assert list(vdf.columns) == ['Date', 'Volume', 'Price_change'], "columns should be named 'Date', 'Volume', 'Price_Change'"
     assert vdf['Volume'].min() >= 0, 'trading volumes should be positive'
     
 
